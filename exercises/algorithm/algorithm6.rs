aac8e36fc7af6eb2e26044a578403e07e864eda7
cloudyi <lyt.ccie@outlook.com>
2025-10-19 10:53:58 +0800

    update

--- exercises/algorithm/algorithm6.rs
+++ exercises/algorithm/algorithm6.rs
@@ -23,7 +23,16 @@ impl Graph {
     }
 
     fn dfs_util(&self, v: usize, visited: &mut HashSet<usize>, visit_order: &mut Vec<usize>) {
-        //TODO
+        // Mark the current node as visited
+        visited.insert(v);
+        visit_order.push(v);
+
+        // Recursively visit all unvisited neighbors
+        for &neighbor in &self.adj[v] {
+            if !visited.contains(&neighbor) {
+                self.dfs_util(neighbor, visited, visit_order);
+            }
+        }
     }
 
     // Perform a depth-first search on the graph, return the order of visited nodes
